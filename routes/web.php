f05cbe6059b1e9b6f04c2f4c2941746f15850449
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-03 17:19:30 +0900

    レイアウトの調整

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/', 'main');
+Route::view('/products', 'products');
+Route::view('/product', 'product');
+Route::view('/cart', 'cart');
+Route::view('/checkout', 'checkout');
+Route::view('/thankyou', 'thankyou');
