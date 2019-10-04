470f40899990ad185d6c3ae85616d0d8e68f59da
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-04 15:23:38 +0900

    画像の追加

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'LandingPageController@index')->name('landing-page');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/shop', 'ShopController@index')->name('shop.index');
+Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-
-Route::view('/', 'main');
-Route::view('/products', 'products');
-Route::view('/product', 'product');
 Route::view('/cart', 'cart');
 Route::view('/checkout', 'checkout');
 Route::view('/thankyou', 'thankyou');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ const mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/sass/responsive.scss', 'public/css')
+   .sourceMaps()
+   .browserSync({
+    proxy: {
+        target: "http://127.0.0.1:8000/",
+        ws: true
+    },
+    browser: "google chrome",
+    // files: [
+    //    './**/*.css',
+    //    './app/**/*',
+    //    './config/**/*',
+    //    './resources/views/**/*.blade.php',
+    //    './resources/views/*.blade.php',
+    //    './routes/**/*'
+    // ],
+    watchOptions: {
+      usePolling: true,
+      interval: 100
+    },
+    open: "external",
+    reloadOnRestart: true
+});
