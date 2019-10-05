a5632accbab0590bff53a30585791e641e488ad5
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-05 13:59:45 +0900

    vue-routerの設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,15 @@
 
 require('./bootstrap');
 
+import Vue from 'vue'
 window.Vue = require('vue');
+
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+import VuePageTransition from 'vue-page-transition'
+Vue.use(VuePageTransition)
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +28,19 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('language-switcher', require('./components/LanguageSwitcher.vue').default);
+Vue.component('language-switcher', require('./components/Transition.vue').default);
+
+
+// vue-routerのインスタンス化、オプションroutesでアクセスされるパスとその時に表示するComponentを指定
+// const router = new VueRouter({
+//     mode: 'history',
+//     routes: [
+//         // TOPページ
+//         { path: '/', component: require('./components/Articles/Index.vue') },
+//         // 記事投稿フォームページ
+//         { path: '/article/create', component: require('./components/Articles/Create.vue') },
+//     ]
+// });
 
 
 /**
@@ -29,6 +49,8 @@ Vue.component('language-switcher', require('./components/LanguageSwitcher.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+
 const app = new Vue({
+    router,
     el: '#app',
 });
---
+++ resources/js/components/Transition.vue
@@ -0,0 +1,19 @@
+<template>
+    <vue-page-transition name="overlay-down-full">
+        <router-view />
+    </vue-page-transition>
+</template>
+
+<script>
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Transition',
+      component: Transition,
+      // overwrite default transition
+      meta: { transition: 'overlay-down-full' },
+    },
+  ]
+})
+</script>
