a6b7856687868937ec9ff4e7813b804ee5931930
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-07 22:16:27 +0900

    heroku のセットアップ

--- public/index.php
+++ public/index.php
@@ -58,3 +58,32 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+$dbopts = parse_url(getenv('http://localhost:8888/phpMyAdmin/db_structure.php?server=1&db=eshop'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'mysql',
+                   'user' => $dbopts["root"],
+                   'password' => $dbopts["root"],
+                   'host' => $dbopts["127.0.0.1"],
+                   'port' => $dbopts["3306"],
+                   'dbname' => ltrim($dbopts["path"],'/eshop')
+                   )
+               )
+);
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
