29878864859fe6c7b25ef35746f235132e67e7e8
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-11 15:44:40 +0900

    voyagerエラー対処

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,10 +2,5 @@
     <div class="footer-content container">
         <div class="made-with">Made by Tana using Laravel and Vue.js</div>
         {{ menu('footer', 'partials.menus.footer') }}
-        {{-- <ul>
-            <li>Follow Me: </li>
-            <li><a href="https://github.com/Shun-ichiroTanaka" target="_blank"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://twitter.com/tana_fe" target="_blank"><i class="fa fa-twitter"></i></a></li>
-        </ul> --}}
     </div> <!-- end footer-content -->
 </footer>
--- resources/views/partials/menus/footer.blade.php
+++ resources/views/partials/menus/footer.blade.php
@@ -3,17 +3,6 @@
         @if ($menu_item->title === 'Follow Me:')
             <li>{{ $menu_item->title }}</li>
         @endif
-        <li><a href="{{ $menu_item->link() }}" target="_blank"><i class="{{ $menu_item->title }}"></i></a></li>
+        <li><a href="{{ $menu_item->link() }}"><i class="fa {{ $menu_item->title }}"></i></a></li>
     @endforeach
 </ul>
-
-{{-- <footer>
-    <div class="footer-content container">
-        <div class="made-with">Made by Tana using Laravel and Vue.js</div>
-        <ul>
-            <li>Follow Me: </li>
-            <li><a href="https://github.com/Shun-ichiroTanaka" target="_blank"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://twitter.com/tana_fe" target="_blank"><i class="fa fa-twitter"></i></a></li>
-        </ul>
-    </div> <!-- end footer-content -->
-</footer> --}}
