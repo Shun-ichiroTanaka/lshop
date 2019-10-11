6761d8cc7e85b8070744db694c12e593dd168605
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-12 00:44:58 +0900

    webpack.mix.jsの修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/responsive.scss', 'public/css')
-   .browserSync()
+   .sourceMaps()
+   .browserSync('http://127.0.0.1:8000/');
