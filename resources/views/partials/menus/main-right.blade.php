7c871ac2b85ad5010fb4b822f6484650c51c4ece
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-11 22:32:54 +0900

    webpackをいじることによるブラウザでのスタイル変更の阻止

---
+++ resources/views/partials/menus/main-right.blade.php
@@ -0,0 +1,20 @@
+<ul>
+   <li><a href="{{ route('register') }}">Sign Up</a></li>
+   <li><a href="{{ route('register') }}">Login</a></li>
+   <li><a href="{{ route('register') }}">Cart
+   <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+   </a></li>
+
+   {{-- @foreach($items as $menu_item)
+       <li>
+           <a href="{{ $menu_item->link() }}">
+               {{ $menu_item->title }}
+               @if ($menu_item->title === 'Cart')
+                   @if (Cart::instance('default')->count() > 0)
+                   <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+                   @endif
+               @endif
+           </a>
+       </li>
+   @endforeach --}}
+</ul>
