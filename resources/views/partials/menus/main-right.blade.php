f991ed4429da3d588db84027e202e38ae5fb1d5f
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-12 00:27:50 +0900

    auth 設定

--- resources/views/partials/menus/main-right.blade.php
+++ resources/views/partials/menus/main-right.blade.php
@@ -4,7 +4,7 @@
         <li><a href="{{ route('register') }}">Login</a></li>
     @else
         <li>
-            <a class="dropdown-item" href="{{ route('logout') }}"
+            <a href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
